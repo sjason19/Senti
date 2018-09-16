c7f51ae93df824479f9ecd9c6ab5ccb7721eb52e
Karn Rahal <karn.rahal@alumni.ubc.ca>
2018-09-15 21:46:21 -0700

    Allows users to use webcam in chrome extension by allowing permission in options page

--- popup.js
+++ popup.js
@@ -131,3 +131,31 @@ document.addEventListener('DOMContentLoaded', function () {
   // Load any CSS that may have previously been saved.
   console.log("at event listener");
 });
+
+document.querySelector('#go-to-options').addEventListener("click", function () {
+  if (chrome.runtime.openOptionsPage) {
+    chrome.runtime.openOptionsPage();
+  } else {
+    window.open(chrome.runtime.getURL('options.html'));
+  }
+});
+
+const player = document.getElementById('player');
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext('2d');
+const captureButton = document.getElementById('capture');
+
+const constraints = {
+  video: true,
+};
+
+captureButton.addEventListener('click', () => {
+  // Draw the video frame to the canvas.
+  context.drawImage(player, 0, 0, canvas.width, canvas.height);
+});
+
+// Attach the video stream to the video element and autoplay.
+navigator.mediaDevices.getUserMedia(constraints)
+  .then((stream) => {
+    player.srcObject = stream;
+  });
